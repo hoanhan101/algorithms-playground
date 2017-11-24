45836b29952e5b99c86da10909b9f14ae9b14aff
Hoanh An <hoanhan101@gmail.com>
2017-11-24 16:49:00 -0500

    no message

---
+++ python_security/port_scanner.py
@@ -0,0 +1,18 @@
+import optparse
+
+parser = optparse.OptionParser('usage %prog -H' + \
+                               '<target host> -p <target port>')
+parser.add_option('-H', dest='tgtHost', type='string', \
+                  help='specify target host')
+parser.add_option('-p', dest='tgtPort', type='int', \
+                  help='specify target port')
+
+(options, args) = parser.parse_args()
+tgtHost = options.tgtHost
+tgtPort = options.tgtPort
+if (tgtHost == None) | (tgtPort == None):
+    print(parser.usage)
+    exit(0)
+
+if __name__ == '__main__':
+    print("Hello World")
