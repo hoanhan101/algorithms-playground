b7871f4fc2266aa988c815e07c31a8ba77c2f577
Hoanh An <hoanhan101@gmail.com>
2017-12-24 10:19:15 -0500

    moved to diffrent dir

--- python_networking/geo_env/get_host_by_name.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# Turning a Hostname into an IP Address
-
-import socket
-
-if __name__ == '__main__':
-    hostname = 'www.python.org'
-    addr = socket.gethostbyname(hostname)
-    print('The IP address of {} is {}'.format(hostname, addr))
